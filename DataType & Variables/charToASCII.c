d5b21cab995adad92230955ad46401b9c649d54f
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-02-06 16:44:04 +0530

    Create charToASCII.c

---
+++ DataType & Variables/charToASCII.c
@@ -0,0 +1,20 @@
+// Code to convert caharacter to it's ASCII value
+// H.W -> Check if it is possible to convert number into its correscponding ASCII caharacter
+
+// 
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    i=34;
+    
+    printf("The value in variable i is: %d", i);
+        
+    // printf("Give me a caharacter: ");
+    // scanf("%c", &chr);
+    
+    // printf("The caharacter you entered is: %c\n", chr);
+    // printf("ASCII value of this caharacter is: %d\n", chr);
+    return 0;
+}
