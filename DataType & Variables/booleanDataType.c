af421382e3a9bacaca9c09a6834849eb72a183d0
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-02-06 22:49:33 +0530

    Create booleanDataType.c

---
+++ DataType & Variables/booleanDataType.c
@@ -0,0 +1,17 @@
+// Boolean Data Types
+// Values possible are :
+        // True 
+        // False
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    bool a = true;
+    bool b = false;
+    
+    printf("True : %d\n",a);
+    printf("False : %d\n",b);
+
+    return 0;
+}
