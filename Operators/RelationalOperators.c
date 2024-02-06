94ad5500e80c79cb72eef452e621b9b3e74b3ec7
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-02-06 22:48:53 +0530

    Create RelationalOperators.c

---
+++ Operators/RelationalOperators.c
@@ -0,0 +1,23 @@
+// RELATIONAL/COMPARISON Operators
+// < -> less than a<b
+// > -> Greater than a>b
+// <= -> Less than, equals to a<=b
+// >= -> Greater than, equals to a>=b
+// == -> Equals to -> a == b
+// != -> Not equals to -> a != b
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    int b = 5;
+    
+    printf("a<b = %d", a<b);
+    printf("\na>b = %d", a>b);
+    printf("\na<=b = %d", a<=b);
+    printf("\na>=b = %d", a>=b);
+    printf("\na==b = %d", a==b);
+    printf("\na!=b = %d", a!=b);
+
+    return 0;
+}
