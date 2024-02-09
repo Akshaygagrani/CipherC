8c16a7aa7def1c6dc44a354514ea9d48b69eae09
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-02-09 10:21:52 +0530

    Create nestedIfElse.c

---
+++ ControlStructures/nestedIfElse.c
@@ -0,0 +1,26 @@
+//Example of  Nested If..Else
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    bool father = false;
+    bool mother = false;
+    
+    if(father){
+        // Father of Aakash has agreed for him to go to GOA....
+        if(mother){
+            // Father as well as agreed of Aakash has agreed for him to go to GOA
+            printf("Yipeee GOA is on guys !!!!");
+        }else{
+            // When Father has agreed but mother has disagreed
+            printf("Sorry Guys let's plan some other time");
+        }
+    }else{
+        // Father has disagreed 
+        printf("Sorry Guys My father has disagreed for this");
+    }
+    
+    return 0;
+}
