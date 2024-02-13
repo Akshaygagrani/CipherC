8d9b8f9efc12052e1d16b5c39d5af3eef118e1ec
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-02-13 09:13:53 +0530

    Create printEven.c

---
+++ loops/printEven.c
@@ -0,0 +1,17 @@
+// Program to print the even numbers from 0 to 20
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    
+    // for(i=0; i<=20; i+=2){
+    //     printf("number is %d\n", i);
+    // }
+    
+    for(i=0; i>=-20; i-=2){
+        printf("number is %d\n", i);
+    }
+
+    return 0;
+}
