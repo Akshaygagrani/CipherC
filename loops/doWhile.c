dc44daeaabd4a05d92b9a559c1717b80151102d5
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-02-13 09:13:37 +0530

    Create doWhile.c

---
+++ loops/doWhile.c
@@ -0,0 +1,22 @@
+// .. Do while loop 
+
+#include <stdio.h>
+
+int main()
+{
+    int x = 0;
+    
+    do{
+       printf("Hello World %d times\n", x); 
+       x++;
+    }while(x<=10);
+    
+    
+    
+    // while(x<=10){
+    //   printf("Hello World");  
+    // }
+    
+
+    return 0;
+}
