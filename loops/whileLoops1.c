a99239ce0f3d1952c94513a84e8945a2c296117e
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-02-13 09:13:26 +0530

    Create whileLoops1.c

---
+++ loops/whileLoops1.c
@@ -0,0 +1,26 @@
+// While loop 
+// Syntax: 
+
+    // Step 1-: counter variable inititialisation
+    // while(Step 2 -: <condition>){
+    //     ...The block of code that you want to loop over
+    //     Step 3 -: Increment/Decrement counter
+    // }
+    
+#include <stdio.h>
+
+int main()
+{
+   
+   x = 11
+    int x = 0;
+    
+    while(x<=10){
+        printf("Hey Doramamu, Do you agree to bargain Y/N %d time ? \n", x);
+        x++;
+    }
+    
+    printf("Dr. Stranger saved us !!");
+
+    return 0;
+}
