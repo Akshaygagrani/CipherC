09e85b29fe35a1b404a7666918128a0f0da8ec2d
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-02-21 10:31:35 +0530

    Update whileIntro.c

--- while/whileIntro.c
+++ while/whileIntro.c
@@ -1,26 +1,23 @@
 // While loop 
 // Syntax: 
-    // while(<condition>){
+
+    // Step 1-: counter variable inititialisation
+    // while(Step 2 -: <condition>){
     //     ...The block of code that you want to loop over
+    //     Step 3 -: Increment/Decrement counter
     // }
-    
-// while(condition){
-//     ...
-//     ..
-//     ...
-//     ..
-// }
     
 #include <stdio.h>
 
 int main()
 {
-    char ans = 'N';
-    printf("Hey Doramamu, Do you agree to bargain Y/N ? \n");
+   
+   x = 11
+    int x = 0;
     
-    while(ans =='N'){
-        scanf("%c", &ans);
-        printf("Answer is : %c", ans);
+    while(x<=10){
+        printf("Hey Doramamu, Do you agree to bargain Y/N %d time ? \n", x);
+        x++;
     }
     
     printf("Dr. Stranger saved us !!");
