c5e6aff223798e26471926dad9c6e44d20f16e6e
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-02-21 10:34:41 +0530

    Create forIntro.c

---
+++ loops/forIntro.c
@@ -0,0 +1,24 @@
+// For Loops
+// Step 1-: counter variable inititialisation
+    // while(Step 2 -: <condition>){
+    //     ...The block of code that you want to loop over
+    //     Step 3 -: Increment/Decrement counter
+    // }
+    
+// Syntax:
+//     for(inititialisationStatement; <condition>; updateStatement){
+//         ...   Looping Code
+//     }
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    
+    i : 11
+    for(i=1; i<=10; i++){
+        printf("Hello World %d times\n", i);
+    }
+
+    return 0;
+}
