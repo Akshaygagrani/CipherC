24be09562b6c7ce966020a4167231bffdad4cfaa
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-02-21 10:40:24 +0530

    Create ternaryOperator.c

---
+++ ControlStructures/ternaryOperator.c
@@ -0,0 +1,20 @@
+// Ternary Operator
+// Syntax:
+//      (condition) ? (trueCodeBlock) : (falseCodeBlock)
+#include <stdio.h>
+
+int main()
+{
+    int m = 5;
+    int n = 14;
+    
+    // if(m>n){
+    //     printf("M is greater than N");
+    // }else{
+    //     printf("N is greater than M");
+    // }
+    
+    (m>n) ? printf("m is greater than n") :  ;
+
+    return 0;
+}
