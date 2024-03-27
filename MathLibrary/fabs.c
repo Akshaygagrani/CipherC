80c84b4d11a7c1c7bce6aa64e0bf3c3c09b09fc7
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-03-27 11:36:40 +0530

    Create fabs.c

---
+++ MathLibrary/fabs.c
@@ -0,0 +1,14 @@
+#include <math.h> 
+#include <stdio.h> 
+
+int main() 
+{ 
+	int a, b; 
+	a = 1234; 
+	b = -344; 
+
+	printf("The absolute value of %d is %lf\n", a, fabs(a)); 
+	printf("The absolute value of %d is %lf\n", b, fabs(b)); 
+
+	return (0); 
+}
