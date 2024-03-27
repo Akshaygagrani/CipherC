55c60835863858dba548a74779d7b2ba9567e92d
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-03-27 12:16:11 +0530

    Update stringIntro.c

--- String/stringIntro.c
+++ String/stringIntro.c
@@ -1,33 +1,51 @@
-// Strings in C -: 
-// Characters -: "a, b, c, d --- z", "A-Z", 
-// String -: it is just a sequence(combination) of caharacters, terminated with a 
-//             null caharacter ("\0")
-            
-// String s = "Akshay"
-// Array of caharacters -: ['A','k','s','h','a','y','\0']
+// Strings in C:
+// caharacters -: (a, b, c, d....z),(A-Z), (0- 9), $, %,.......
+// It is just a sequence of caharacters, terminated with '\0' (null/EOF caharacter)
+
+// Example -:
+// name = "Akshay"
+// cahracterstics = "The Best"
+
+// name = ['A', 'k', 's', 'h', 'a', 'y', '\0']
+
+
+// c1 = 'A'
+// c2 = 'k'
+// c3 = 's'
+// c4  ..
+// c5  ...
+// c6  ....
 
 // Syntax -: 
-//         1st way -:
-//         char c[] = "c Schools";
-//         c = ['c', ' ', 'S', 'c', 'h', 'o', 'o', 'l', 's', '\0'];
-        
-//         Way to initialise the string -:
-//         char c[] = "abcd";
-//         char c[10] = "abcd";
-//         char c[] = {'a', 'b', 'c', 'd', '\0'}
-//         char c[10] = {'a', 'b', 'c', 'd', '\0'}
-        
-// " " <- double quotes 
+
+//     int age[] = {1, 2, 3, 4, 5}; 
+// // 1st Way
+//     char name[] = "Akshay"
+//     name = ['A', 'k', 's', 'h', 'a', 'y', '\0']
+
+// // 2nd Way
+//     char c[] = "Akshay"
+//     char c[20] = "Akshay"
+//     char c[] = {'A', 'k', 's', 'h', 'a', 'y', '\0'}
+//     char c[20] = {'A', 'k', 's', 'h', 'a', 'y', '\0'}
     
-    
+// Double quotes
+// " " <- Double quotes specifies that the word/part of the code is a string
+
+
 #include <stdio.h>
+
 int main()
 {
-    char c[100];
-    c = "Cipher Schools"; // ASSIGNMENT OPERATORS NOT ALLOWED
+    char name[] = "Akshay";
     
+    printf("My name is %s", name);
+
     return 0;
 }
+
+
+
 
 
 
