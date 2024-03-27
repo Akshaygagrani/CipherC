f30d787e09a7354905d3c8a46bb2ef2a194dd6cf
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-03-27 12:15:54 +0530

    Create stringIntro.c

---
+++ String/stringIntro.c
@@ -0,0 +1,35 @@
+// Strings in C -: 
+// Characters -: "a, b, c, d --- z", "A-Z", 
+// String -: it is just a sequence(combination) of caharacters, terminated with a 
+//             null caharacter ("\0")
+            
+// String s = "Akshay"
+// Array of caharacters -: ['A','k','s','h','a','y','\0']
+
+// Syntax -: 
+//         1st way -:
+//         char c[] = "c Schools";
+//         c = ['c', ' ', 'S', 'c', 'h', 'o', 'o', 'l', 's', '\0'];
+        
+//         Way to initialise the string -:
+//         char c[] = "abcd";
+//         char c[10] = "abcd";
+//         char c[] = {'a', 'b', 'c', 'd', '\0'}
+//         char c[10] = {'a', 'b', 'c', 'd', '\0'}
+        
+// " " <- double quotes 
+    
+    
+#include <stdio.h>
+int main()
+{
+    char c[100];
+    c = "Cipher Schools"; // ASSIGNMENT OPERATORS NOT ALLOWED
+    
+    return 0;
+}
+
+
+
+
+
