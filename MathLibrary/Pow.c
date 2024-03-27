66cdd7aa44fe8850675b3e04b0328c23552c84d9
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-03-27 11:38:48 +0530

    Create Pow.c

---
+++ MathLibrary/Pow.c
@@ -0,0 +1,11 @@
+#include <math.h> 
+#include <stdio.h> 
+
+int main() 
+{ 
+	printf("Value 8.0 ^ 3 = %lf\n", pow(8.0, 3)); 
+
+	printf("Value 3.05 ^ 1.98 = %lf", pow(3.05, 1.98)); 
+
+	return (0); 
+}
