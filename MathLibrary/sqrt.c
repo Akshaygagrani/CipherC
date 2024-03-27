9402bbfff24a8d33e6665663dc38894fa8d7e423
Akshay Gagrani <gagrani.akshay@gmail.com>
2024-03-27 11:38:31 +0530

    Create sqrt.c

---
+++ MathLibrary/sqrt.c
@@ -0,0 +1,13 @@
+#include <math.h> 
+#include <stdio.h> 
+
+int main() 
+{ 
+
+	printf("Square root of %lf is %lf\n", 225.0, 
+		sqrt(225.0)); 
+	printf("Square root of %lf is %lf\n", 300.0, 
+		sqrt(300.0)); 
+
+	return (0); 
+}
